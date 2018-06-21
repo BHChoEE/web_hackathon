00e87b478a3f71ee47cf20c45ab9e0ab9fb01cfe
BHChoEE <b03901022@ntu.edu.tw>
2018-06-21 21:35:08 +0800

    server mod

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 var http = require('http');
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 // setup server
 const app = express();
 const server = http.createServer(app);
 app.use(express.static(path.join(__dirname ,'public')));
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 // render an API index page
-app.get(['/', '/index'], function(req, res){
-	res.sendFile(path.join(__dirname, './public/index.html'));
+app.get('/*', function(req, res){
+	res.sendFile(path.join(__dirname, '/public/index.html'), function(err){
+		if(err){res.status(500).send(err);}
+	});
 });
 
 server.listen(3000);
