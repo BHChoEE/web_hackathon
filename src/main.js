ea35e38900b435d975a082968d7aeb3e8a17e1b9
Kevin <b03902086@ntu.edu.tw>
2018-06-21 23:43:54 +0800

    add citation list and show checked papers in drawer

--- src/main.js
+++ src/main.js
@@ -5,8 +5,37 @@ import PaperList from './paperList';
 import ReferenceList from './refList';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Switch from '@material-ui/core/Switch';
-// import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import { ListItem, ListItemText, ListItemIcon } from '@material-ui/core';
 const JSSoup = require('jssoup').default;
+
+const styles = {
+    root: {
+        flexGrow: 1,
+    },
+    flex: {
+        flex: 1,
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
+    },
+    drawer: {
+        width: 500,
+    },
+};
 
 class Main extends React.Component {
     constructor() {
@@ -15,8 +44,11 @@ class Main extends React.Component {
             query: "",
             paperList: [],
             referenceList: [],
-            onlyInfluential: false,
+            citationList: [],
+            onlyInfluentialRefs: false,
+            onlyInfluentialCits: false,
             checkedList: [],
+            drawerOpen: false,
         }
         this.updateQuery = this.updateQuery.bind(this);
         this.sendQuery = this.sendQuery.bind(this);
@@ -49,6 +81,7 @@ class Main extends React.Component {
             this.setState({
                 paperList: paperList,
                 referenceList: [],
+                citationList: [],
             });
         })
         .catch(error => {
@@ -60,32 +93,37 @@ class Main extends React.Component {
         this.setState({query: query});
     }
 
-    updateOnlyInfluential(e) {
-        this.setState({onlyInfluential: e.target.checked});
+    updateOnlyInfluential = onlyInfluential => e => {
+        this.setState({[onlyInfluential]: e.target.checked});
     }
 
     handleChooseTitle(title, id, info) {
         axios.get("https://api.semanticscholar.org/v1/paper/" + id + "?include_unknown_references=false")
         .then(response => {
             var referenceList = [];
-            response.data.references.forEach(ref => {
-                var info = ref.year;
-                if (ref.venue) {
-                    info += ", " + ref.venue;
-                }
-                var reference = {
-                    title: ref.title,
-                    id: ref.paperId,
-                    isInfluential: ref.isInfluential,
-                    info: info,
-                };
-                if (ref.isInfluential) {
-                    referenceList.unshift(reference);
-                }
-                else {
-                    referenceList.push(reference);
-                }
-            });
+            var citationList = [];
+            var makeList = (src, dst) => {
+                src.forEach(paper => {
+                    var info = paper.year;
+                    if (paper.venue) {
+                        info += ", " + paper.venue;
+                    }
+                    var paperObj = {
+                        title: paper.title,
+                        id: paper.paperId,
+                        isInfluential: paper.isInfluential,
+                        info: info,
+                    };
+                    if (paper.isInfluential) {
+                        dst.unshift(paperObj);
+                    }
+                    else {
+                        dst.push(paperObj);
+                    }
+                });
+            }
+            makeList(response.data.references, referenceList);
+            makeList(response.data.citations, citationList);
             var paperList = [...this.state.paperList];
             var paper = {
                 title: title.replace("\n", " "),
@@ -107,6 +145,7 @@ class Main extends React.Component {
             }
             this.setState({
                 referenceList: referenceList,
+                citationList: citationList,
                 paperList: paperList,
             });
         })
@@ -127,43 +166,125 @@ class Main extends React.Component {
         this.setState({checkedList: newCheckedList});
     }
     
+    toggleDrawer = (state) => () => {
+        this.setState({
+            drawerOpen: state,
+        });
+    }
+    
     render() {
-        return (
-            <div>
-                <UserInput
-                    sendQuery={this.sendQuery}
-                    query={this.state.query}
-                    updateQuery={this.updateQuery}
-                />
-                <FormControlLabel
-                    control={
-                        <Switch
-                            checked={this.state.onlyInfluential}
-                            onChange={this.updateOnlyInfluential}
-                            color="primary"
-                        />
-                    }
-                    label="Only influential"
-                />
-                {
-                    this.state.paperList.length > 0 &&
-                    <PaperList
-                        paperList={this.state.paperList}
+        const { classes } = this.props;
+        var checkedList = this.state.checkedList.map(title => (
+            <ListItem>
+              <ListItemText primary={title} />
+            </ListItem>
+        ));
+        var appBar = (
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton className={classes.menuButton} onClick={this.toggleDrawer(true)} color="inherit" aria-label="Menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="title" color="inherit" className={classes.flex}>
+                        Paper Query
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        );
+        var drawer = (
+            <Drawer open={this.state.drawerOpen} onClose={this.toggleDrawer(false)}>
+                <div
+                    tabIndex={0}
+                    role="button"
+                    onClick={this.toggleDrawer(false)}
+                    onKeyDown={this.toggleDrawer(false)}
+                >
+                    <div className={classes.drawer}>
+                        <List>
+                            {checkedList}
+                        </List>
+                    </div>
+                </div>
+            </Drawer>
+        );
+        var userInput = (
+            <UserInput
+                sendQuery={this.sendQuery}
+                query={this.state.query}
+                updateQuery={this.updateQuery}
+            />
+        );
+        var paperList = this.state.paperList.length == 0 ? null : (
+            <PaperList
+                paperList={this.state.paperList}
+                handleChooseTitle={this.handleChooseTitle}
+                handleToggleChecked={this.handleToggleChecked}
+                checkedList={this.state.checkedList}
+            />
+        );
+        var referenceList = (
+            <Grid item xs={12} sm={6}>
+                <Paper className={classes.paper}>
+                    <FormControlLabel
+                        control={
+                            <Switch
+                                checked={this.state.onlyInfluentialRefs}
+                                onChange={this.updateOnlyInfluential("onlyInfluentialRefs")}
+                                color="primary"
+                            />
+                        }
+                        label="Only influential"
+                    />
+                    <ReferenceList
+                        referenceList={this.state.citationList}
                         handleChooseTitle={this.handleChooseTitle}
+                        onlyInfluential={this.state.onlyInfluentialRefs}
                         handleToggleChecked={this.handleToggleChecked}
                         checkedList={this.state.checkedList}
                     />
-                }
-                <ReferenceList
-                    referenceList={this.state.referenceList}
-                    handleChooseTitle={this.handleChooseTitle}
-                    onlyInfluential={this.state.onlyInfluential}
-                    handleToggleChecked={this.handleToggleChecked}
-                    checkedList={this.state.checkedList}
-                />
+                </Paper>
+            </Grid>
+        );
+        var citationList = (
+            <Grid item xs={12} sm={6}>
+                <Paper className={classes.paper}>
+                    <FormControlLabel
+                        control={
+                            <Switch
+                                checked={this.state.onlyInfluentialCits}
+                                onChange={this.updateOnlyInfluential("onlyInfluentialCits")}
+                                color="primary"
+                            />
+                        }
+                        label="Only influential"
+                    />
+                    <ReferenceList
+                        referenceList={this.state.referenceList}
+                        handleChooseTitle={this.handleChooseTitle}
+                        onlyInfluential={this.state.onlyInfluentialCits}
+                        handleToggleChecked={this.handleToggleChecked}
+                        checkedList={this.state.checkedList}
+                    />
+                </Paper>
+            </Grid>
+        );
+        return (
+            <div className={classes.root}>
+                <Grid container spacing={24}>
+                    {appBar}
+                    {drawer}
+                    {userInput}
+                    {paperList}
+                    {referenceList}
+                    {citationList}
+                </Grid>
             </div>
         );
     }
 }
 
-export default Main;
+Main.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Main);
